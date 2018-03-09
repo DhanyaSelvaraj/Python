e1517da3bfff19bac35a5d29c02b880cb36f0ce0
Dhanya Rajakumari <34230161+DhanyaSelvaraj@users.noreply.github.com>
2018-03-09 14:27:46 +0530

    Add files via upload

---
+++ excolor.py
@@ -0,0 +1,8 @@
+color=["red","orange","yellow","black","blue"]
+colorselect=input("select the color = ")
+#while (colorselect)!="stop":
+#colorselect=input("select the color =")
+if(color.count(colorselect)>=1):
+        print("color availale")
+elif(colorselect)!="stop":
+        print("color is not availale")
