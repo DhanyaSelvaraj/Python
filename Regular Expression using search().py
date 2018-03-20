5ceb9468fb57d5e6fad3a6913f7bb5daf5a1dbfd
Dhanya Rajakumari <34230161+DhanyaSelvaraj@users.noreply.github.com>
2018-03-20 14:25:37 +0530

    Update Regular Expression using search().py

--- Regular Expression using search().py
+++ Regular Expression using search().py
@@ -1,8 +1,23 @@
 Regular Expression(search):
 
+Program-1:
+    
 import re
 if(re.search("happy","I am very happy today")):
     print("There is happy")
 
 #Output:
     There is happy
+    
+Pogram-2:
+    
+import re
+myno="91-987-654-3210"
+if re.search(r"\w{2}-\w{3}-\w{3}-\w{4}",myno):
+    print("Valid number")
+else:
+    print("Notvalid number")
+
+#Output:
+    Valid number
+
